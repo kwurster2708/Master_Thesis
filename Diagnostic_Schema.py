0beeb647ff6d3602c4a4280aad9ae13b5ff49580
Thesis_Kim <kimwurster24@gmail.com>
2026-02-21 17:49:39 +0100

    First Commit

---
+++ Diagnostic_Schema.py
@@ -0,0 +1,288 @@
+import sqlite3, json
+
+conn = sqlite3.connect(
+        r"\Users\Kim_W\Ekkono_Code\WeatherData.sqlite"
+    )
+
+"""
+Diagnostic Schema:
+
+{
+Tag ID: ,
+Device ID: ,
+Local Model ID: , 
+Model Binary ID: ,
+Timestamp: ,
+Anomaly: {
+	Outlier Score Value: ,
+	Outlier Score Function:
+},
+Performance: {
+	MAE: ,
+    MAPE: ,
+    RMSE: 
+},
+Tag Information: {
+    Outlier Tag: {
+	    Key: ,
+	    Value:
+     },
+     Other Device Tags: {
+        Key: ,
+        Value:
+     }
+},
+Local Model Information: {
+	Metrics: {
+	    Name: ,
+	    Value: 
+	},
+    last_train_time: , 
+    is_compatible: , 
+    is_valid
+},
+Model Binary Information: {
+    is_deserializable: ,
+    contains_statistics: ,
+    is_single_target: ,
+    is_producing_predictions:
+},
+Attribute Sensitivities
+}
+"""
+
+#---HELPER FUNCTIONS FOR DIAGNOSTIC SCHEMA ANALYSIS---#
+
+def outlier_information(conn, tag_id, device_id, localmodel_id, modelbinary_id, outlier_score_value):
+    """Query outlier important information from modelhealth table"""
+    cur = conn.cursor()
+
+    try:
+        cur.execute(f"""SELECT update_time, outlier_score_value, outlier_score_fn, mae, mape, rmse 
+                    FROM modelhealth 
+                    WHERE tag_id = {tag_id} AND device_id = {device_id} AND localmodel_id = {localmodel_id} AND modelbinary_id = {modelbinary_id} AND outlier_score_value = {outlier_score_value};
+                    """)
+        
+        outlier_information = cur.fetchall()
+
+        for row in outlier_information:
+            timestamp = row[0]
+            anomaly = {"Outlier Score Value": row[1], "Outlier Score Function": row[2]}
+            performance = {"MAE": row[3], "MAPE": row[4], "RMSE": row[5]}
+        return timestamp, anomaly, performance
+    
+    except sqlite3.Error as e:
+        print(f"Error executing outlier query: {e}")
+
+
+def tag_information(conn, tag_id):
+    """Query tag information from tag table"""
+    cur = conn.cursor()
+
+    try:
+        cur.execute(f"""SELECT key, value
+                    FROM tag
+                    WHERE id = {tag_id};
+                    """)
+        
+        tag_information = cur.fetchall()
+
+        for row in tag_information:
+            tags = {f"{row[0]}": row[1]}
+        return tags
+    
+    except sqlite3.Error as e:
+        print(f"Error executing tag information query: {e}")        
+
+def other_tag_information(conn, tag_id, device_id):
+    """Query tag information from tag table"""
+    cur = conn.cursor()
+
+    try:
+        cur.execute(f"""SELECT key, value
+                    FROM tag
+                    WHERE id IN (SELECT tag_id
+                                FROM devicetaglink
+                                WHERE device_id = {device_id} AND tag_id != {tag_id} AND tag_id != 1);
+                    """)
+        
+        tag_information = cur.fetchall()
+        other_tags = {}
+        
+        for row in tag_information:
+            other_tags[f"{row[0]}"] = row[1]
+        return other_tags
+    
+    except sqlite3.Error as e:
+        print(f"Error executing tag information query: {e}")        
+
+def metric_information(conn, localmodel_id):
+    """Query metric information from metric table"""
+    cur = conn.cursor()
+
+    try:
+        cur.execute(f"""SELECT name, value
+                    FROM metric
+                    WHERE localmodel_id = {localmodel_id}; 
+                    """)
+        
+        metric_information = cur.fetchall()
+        metric = {}
+        
+        for row in metric_information:
+            metric[row[0]] = row[1]
+        return metric
+
+    except sqlite3.Error as e:
+        print(f"Error executing metric information query: {e}")
+        
+def localmodel_information(conn, localmodel_id):
+    """Query local model information from local model table"""
+    cur = conn.cursor()
+
+    try:
+        cur.execute(f"""SELECT last_train_time, is_compatible, is_valid
+                    FROM localmodel
+                    WHERE id = {localmodel_id}; 
+                    """)
+        
+        localmodel_information = cur.fetchall()
+
+        for row in localmodel_information:
+            last_train_time = row[0]
+            is_compatible = row[1]
+            is_valid = row[2]
+        return last_train_time, is_compatible, is_valid
+    
+    except sqlite3.Error as e:
+        print(f"Error executing local model information query: {e}")
+
+def modelbinary_information(conn, modelbinary_id):
+    """Query modelbinary information from modelbinary table"""
+    cur = conn.cursor()
+
+    try:
+        cur.execute(f"""SELECT is_deserializable, contains_statistics, is_single_target,
+                    is_producing_predictions, attribute_sensitivities_id
+                    FROM modelbinary
+                    WHERE id = {modelbinary_id};
+                    """)
+        
+        modelbinary_information = cur.fetchall()
+
+        for row in modelbinary_information:
+            modelbinary = {"Is Deserializable": row[0], "Contains Statistics": row[1], "Is Single Target": row[2], "Is Producing Predictions": row[3]}
+            attributes_sensitivities_id = row[4]   
+        return modelbinary, attributes_sensitivities_id
+    
+    except sqlite3.Error as e:
+        print(f"Error executing modelbinary information query: {e}")
+        
+def attributes_information(conn, attributes_sensitivities_id):
+    """Query attributes information from attributesensitivities table"""
+    cur = conn.cursor()
+
+    try:
+        cur.execute(f"""SELECT json(attribute_sensitivities)
+                    FROM attributesensitivities
+                    WHERE id = {attributes_sensitivities_id};
+                    """)
+        
+        attributes_information = cur.fetchall()
+
+        for row in attributes_information:
+            attributes = json.loads(row[0])
+        return attributes
+    
+    except sqlite3.Error as e:
+        print(f"Error executing attributes information query: {e}")
+
+
+
+#---DIAGNOSTIC SCHEMA ANALYSIS FUNCTIONS---#
+
+def find_all_outlier_devices(conn):
+    """Query outliers from modelhealth table"""
+    cur = conn.cursor()
+
+    try:
+        cur.execute("""SELECT device_id, localmodel_id, COUNT(*) as device_count
+                    FROM modelhealth
+                    WHERE outlier_score_value < -5
+                    GROUP BY device_id, localmodel_id
+                    ORDER BY device_count DESC;""")
+        outliers = cur.fetchall()
+        
+        for outlier in outliers:
+            print(outlier)
+        return
+    except sqlite3.Error as e:
+        print(f"Error finding all the outliers: {e}")
+        
+def create_single_schema(conn, tag_id, device_id, localmodel_id, modelbinary_id, outlier_score_value):
+    """Create a JSON schema of the database"""
+    timestamp, anomaly, performance = outlier_information(conn, tag_id, device_id, localmodel_id, modelbinary_id, outlier_score_value)
+    tags = tag_information(conn, tag_id)
+    other_tags = other_tag_information(conn, tag_id, device_id)
+    metrics = metric_information(conn, localmodel_id)
+    last_train_time, is_compatible, is_valid = localmodel_information(conn, localmodel_id)
+    modelbinary, attributes_sensitivities_id = modelbinary_information(conn, modelbinary_id)
+    attributes = attributes_information(conn, attributes_sensitivities_id)
+    schema = {
+        "Tag ID": tag_id,
+        "Local Model ID": localmodel_id,
+        "Model Binary ID": modelbinary_id,
+        "Timestamp": timestamp,
+        "Anomaly": anomaly,
+        "Performance": performance,
+        "Tag Information": {
+            "Outlier Tag": tags,
+            "Other Device Tags": other_tags
+            },
+        "Local Model Information": {
+            "Metrics": metrics,
+            "Last Train Time": last_train_time,
+            "Is Compatible": is_compatible,
+            "Is Valid": is_valid
+        },
+        "Model Binary Information": modelbinary,
+        "Attributes Information": attributes
+    }
+    return schema
+
+def create_big_schema(conn, device_id, localmodel_id):
+    """Create a JSON schema for all the outliers in the database"""
+    cur = conn.cursor()
+
+    try:
+        cur.execute(f"""SELECT tag_id, modelbinary_id, outlier_score_value
+                    FROM modelhealth
+                    WHERE device_id = {device_id} AND localmodel_id = {localmodel_id};""")
+        outliers = cur.fetchall()
+        
+        count_outlier = 0
+        count_normal = 0
+        
+        schema = {
+            "Device ID": device_id,
+            "Local Model ID": localmodel_id,
+            "Outliers": {},
+            "Normal Data Points": {}
+        }
+        
+        for row in outliers:
+            if row[2] < -5:
+                count_outlier += 1
+                key = f"Outlier {count_outlier}"
+                value = create_single_schema(conn, row[0], device_id, localmodel_id, row[1], row[2])
+                schema["Outliers"][key] = value
+            else:
+                count_normal += 1
+                key = f"Normal data point {count_normal}"
+                value = create_single_schema(conn, row[0], device_id, localmodel_id, row[1], row[2])
+                schema["Normal Data Points"][key] = value
+        print(schema)
+        return schema
+    
+    except sqlite3.Error as e:
+        print(f"Error creating big schema: {e}")
